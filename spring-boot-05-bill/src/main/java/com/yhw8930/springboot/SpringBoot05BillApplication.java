f135454f35b51a496268776216858b133b8878f8
yhw8930 <1170930576@qq.com>
2019-04-26 12:41:46 +0800

    spring boot servlet容器

---
+++ spring-boot-05-bill/src/main/java/com/yhw8930/springboot/SpringBoot05BillApplication.java
@@ -0,0 +1,14 @@
+package com.yhw8930.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot05BillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot05BillApplication.class, args);
+    }
+
+}
+
