dea7bbbae12b14f933a4d9aa845535ebd1a0a154
yhw8930 <1170930576@qq.com>
2019-04-28 11:49:24 +0800

    spring boot tomcat提交

---
+++ spring-boot-07-tomcat/src/main/java/com/yhw8930/springboot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.yhw8930.springboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBoot07TomcatApplication.class);
+    }
+
+}
