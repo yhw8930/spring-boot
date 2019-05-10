31cbe969a518936164ce4241e256987cb4b7be7b
yhw8930 <1170930576@qq.com>
2019-05-10 20:16:21 +0800

    spring boot mybatis提交

---
+++ spring-boot-08-data-mybatis/src/main/java/com/yhw8930/springboot/SpringBoot08DataMybatisApplication.java
@@ -0,0 +1,16 @@
+package com.yhw8930.springboot;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//会自动装配指定包下面所有Mapper，省得在每个Mapper上面写@Mapper
+@MapperScan("com.yhw8930.springboot.mapper")
+@SpringBootApplication
+public class SpringBoot08DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot08DataMybatisApplication.class, args);
+    }
+
+}
